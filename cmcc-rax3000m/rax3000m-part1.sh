454f8c4c9ecfb953e055fc26919cf4c39e261087
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-28 01:50:49 +0800

    Update rax3000m-part1.sh

--- cmcc-rax3000m/rax3000m-part1.sh
+++ cmcc-rax3000m/rax3000m-part1.sh
@@ -14,16 +14,18 @@
 #See the License for the specific language governing permissions and
 #limitations under the License.
 echo '添加Passwall依赖feeds'
-sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
+#sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
 #
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
 #echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 
+
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
 
 # Add a feed source
 rm -rf package/helloworld
 git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
-
