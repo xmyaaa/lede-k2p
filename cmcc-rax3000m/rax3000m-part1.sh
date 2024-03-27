8fafb960dccc198be0d2d398872cd9accfbad32f
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-28 01:49:47 +0800

    Update rax3000m-part1.sh

--- cmcc-rax3000m/rax3000m-part1.sh
+++ cmcc-rax3000m/rax3000m-part1.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+#
+#Copyright 2021-2024 yangxu52<https://github.com/yangxu52>
+#
+#Licensed under the Apache License, Version 2.0 (the "License");
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+#Unless required by applicable law or agreed to in writing, software
+#distributed under the License is distributed on an "AS IS" BASIS,
+#WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#See the License for the specific language governing permissions and
+#limitations under the License.
+echo '添加Passwall依赖feeds'
+sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
+#
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+#echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+
+echo '=========Add passwall feeds source OK!========='
+
+# Add a feed source
+rm -rf package/helloworld
+git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
 
