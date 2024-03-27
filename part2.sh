f751f6283b3310d5e9dd864a8c7d6cf73dc93abe
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-28 01:24:09 +0800

    Update part2.sh

--- part2.sh
+++ part2.sh
@@ -17,4 +17,4 @@
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
