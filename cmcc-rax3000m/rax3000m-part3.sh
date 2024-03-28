62a06b1d6619f1a166c76ca6766414a261041b2b
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-28 08:49:43 +0800

    Update rax3000m-part3.sh

--- cmcc-rax3000m/rax3000m-part3.sh
+++ cmcc-rax3000m/rax3000m-part3.sh
@@ -15,14 +15,14 @@
 #limitations under the License.
 
 echo '修改主机名'
-sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
+#sed -i "s/hostname='ImmortalWrt'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
+#cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert hostname OK!========='
 
 echo '修改默认IP'
-sed -i 's/ipad=${ipaddr:-"192.168.1.1"}/ipad=${ipaddr:-"192.168.10.1"}/g' package/base-files/files/bin/config_generate
-sed -i 's/addr_offset=2/addr_offset=8/g' package/base-files/files/bin/config_generate
-sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"192.168.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
-cat package/base-files/files/bin/config_generate |grep hostname=
+#sed -i 's/ipad=${ipaddr:-"192.168.1.1"}/ipad=${ipaddr:-"192.168.10.1"}/g' package/base-files/files/bin/config_generate
+#sed -i 's/addr_offset=2/addr_offset=8/g' package/base-files/files/bin/config_generate
+#sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"192.168.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
+#cat package/base-files/files/bin/config_generate |grep hostname=
 echo '=========Alert Default IP OK!========='
 
