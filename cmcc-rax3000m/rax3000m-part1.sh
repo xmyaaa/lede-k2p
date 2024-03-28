c41e31df661e3be375f672a9104d3ff18e94176a
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-03-28 08:51:07 +0800

    Update rax3000m-part1.sh

--- cmcc-rax3000m/rax3000m-part1.sh
+++ cmcc-rax3000m/rax3000m-part1.sh
@@ -17,9 +17,9 @@
 echo '添加Passwall依赖feeds'
 #sed -i '1 i src-git-full passwall https://github.com/xiaorouji/openwrt-passwall-packages;main' feeds.conf.default
 #
-echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
+#echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git' >>feeds.conf.default
 #echo 'src-git Openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo '=========Add passwall feeds source OK!========='
 # Add a feed source
@@ -28,4 +28,4 @@ echo '=========Add passwall feeds source OK!========='
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
