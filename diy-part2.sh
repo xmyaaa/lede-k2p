6768992ddbe95714ee51b2b6f69c8033abc7242f
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-02 20:53:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -47,3 +47,9 @@ echo '修改Passwall2检测规则'
 #sed -i 's/socket" "iptables-mod-//g' feeds/luci/applications/luci-app-passwall2/root/usr/share/passwall2/app.sh
 echo '=========ALTER passwall denpendcies check OK!========='
 
+echo '修改默认IP'
+sed -i 's/ipad=${ipaddr:-"192.168.1.1"}/ipad=${ipaddr:-"192.168.10.1"}/g' package/base-files/files/bin/config_generate
+sed -i 's/addr_offset=2/addr_offset=8/g' package/base-files/files/bin/config_generate
+sed -i 's/${ipaddr:-"192.168.$((addr_offset++)).1"}/${ipaddr:-"192.168.$((addr_offset++)).1"}/g' package/base-files/files/bin/config_generate
+cat package/base-files/files/bin/config_generate |grep hostname=
+echo '=========Alert Default IP OK!========='
