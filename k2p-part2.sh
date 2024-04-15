b18b21649244e0069b5f3cc5861825aefd9b1629
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-15 20:09:15 +0800

    Update k2p-part2.sh

--- k2p-part2.sh
+++ k2p-part2.sh
@@ -26,8 +26,8 @@ sed -i 's/ImmortalWrt/K2P/g' package/base-files/files/bin/config_generate
 #echo 'WNMEnable=1' >> package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
 
 echo '替换Passwall软件'
-rm -rf feeds/luci/applications/luci-app-passwall/*
+# rm -rf feeds/luci/applications/luci-app-passwall/*
 git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
 mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
-rm -rf passwall
+# rm -rf passwall
 echo '=========Replace passwall source OK!========='
