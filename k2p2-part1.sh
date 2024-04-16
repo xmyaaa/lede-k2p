ba3a566ccb3dacd450575f412e55dbd69bc75df5
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-16 09:11:56 +0800

    Update k2p2-part1.sh

--- k2p2-part1.sh
+++ k2p2-part1.sh
@@ -1,1 +1,25 @@
 
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+
+# echo '添加omcproxy软件源'
+#git clone https://github.com/smsmail2022/luci-app-omcproxy package/luci-app-omcproxy
+#git clone -b 18.06 https://github.com/riverscn/luci-app-omcproxy.git package/luci-app-omcproxy
+#git clone https://github.com/riverscn/luci-app-omcproxy.git package/luci-app-omcproxy
