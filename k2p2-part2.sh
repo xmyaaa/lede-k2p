a9c1332a616ac02ecc7d3805a5ae3569b50c46a9
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-16 09:12:34 +0800

    Update k2p2-part2.sh

--- k2p2-part2.sh
+++ k2p2-part2.sh
@@ -1,1 +1,38 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# Modify default Hostname
+#sed -i 's/ImmortalWrt/K2P/g' package/base-files/files/bin/config_generate
+
+#Enable 802.11k/v/r
+#sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
+#sed -i 's/RRMEnable=0/RRMEnable=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
+#sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
+#sed -i 's/FtSupport=0/FtSupport=1/g' package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
+# echo 'WNMEnable=1' >> package/kernel/mt-drivers/mt_wifi/files/mt7615.1.2G.dat
+# echo 'WNMEnable=1' >> package/kernel/mt-drivers/mt_wifi/files/mt7615.1.5G.dat
+#
+echo '替换golang到1.22.x'
+rm -rf feeds/packages/lang/golang
+git clone -b 22.x --single-branch https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+echo '=========Replace golang OK!========='
+
+echo '替换Passwall软件'
+# rm -rf feeds/luci/applications/luci-app-passwall/*
+#git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
+#mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+# rm -rf passwall
+echo '=========Replace passwall source OK!========='
 
