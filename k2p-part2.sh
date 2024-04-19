2a19122bc576f186e5ed71632c316e52f5a8b612
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-19 23:04:58 +0800

    Update k2p-part2.sh

--- k2p-part2.sh
+++ k2p-part2.sh
@@ -27,7 +27,7 @@ sed -i 's/ImmortalWrt/K2P/g' package/base-files/files/bin/config_generate
 
 echo '替换Passwall软件'
 # rm -rf feeds/luci/applications/luci-app-passwall/*
-#git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
-#mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
+git clone -b main --single-branch https://github.com/xiaorouji/openwrt-passwall passwall
+mv passwall/luci-app-passwall/* feeds/luci/applications/luci-app-passwall/
 # rm -rf passwall
 echo '=========Replace passwall source OK!========='
