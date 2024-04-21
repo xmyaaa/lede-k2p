1c6fcecb0f4c8231e10ba130f44d6842c9753184
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-21 21:49:13 +0800

    Update k2p-part1.sh

--- k2p-part1.sh
+++ k2p-part1.sh
@@ -32,7 +32,8 @@ echo '添加Passwall依赖feeds'
 echo '=========Add passwall feeds source OK!========='
 #
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/xmyaaa/helloworld' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
