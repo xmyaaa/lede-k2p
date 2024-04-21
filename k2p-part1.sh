6eea77e92cf56338fbcfbfe0869ec391dcd132a8
xmyaaa <78308785+xmyaaa@users.noreply.github.com>
2024-04-22 01:37:21 +0800

    Update k2p-part1.sh

--- k2p-part1.sh
+++ k2p-part1.sh
@@ -12,8 +12,8 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/src-git helloworld/src-git helloworld/g' ./feeds.conf.default
+sed -i 's/^\(.*helloworld\)/\1/' feeds.conf.default
+#sed -i 's/src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -37,6 +37,7 @@ echo '=========Add passwall feeds source OK!========='
 echo 'src-git helloworld https://github.com/xmyaaa/helloworld.git' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
 #echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+echo "src-git passwall_packages https://github.com/xmyaaa/openwrt-passwall.git;main" >> "feeds.conf.default"
 echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 
 # echo '添加omcproxy软件源'
